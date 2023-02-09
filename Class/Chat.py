ffc34bbb0ae475ed12828566f9c85a2754512957
Roshanen <106764256+Roshanen@users.noreply.github.com>
2023-02-09 22:37:22 +0700

    Create Chat.py

---
+++ Class/Chat.py
@@ -0,0 +1,5 @@
+class Chat:
+    def __init__(self, my_text, them_text, them_profile):
+        self.__my_text = my_text
+        self.__them_text = them_text
+        self.__them_profile = them_profile
